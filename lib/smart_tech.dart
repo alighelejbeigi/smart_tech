37df21dfdff85326e47a34bf2147fbb522647008
alibeigi <alighb30@gmail.com>
2025-10-21 19:10:15 +0330

    wip: add some logic for get data from native

--- lib/smart_tech.dart
+++ lib/smart_tech.dart
@@ -1,8 +1,18 @@
+// lib/smart_tech.dart
 
 import 'smart_tech_platform_interface.dart';
 
 class SmartTech {
+  // این کد از قبل در فایل شما موجود است
+  static SmartTechPlatform get _platform => SmartTechPlatform.instance;
+
+  // متد موجود:
   Future<String?> getPlatformVersion() {
-    return SmartTechPlatform.instance.getPlatformVersion();
+    return _platform.getPlatformVersion();
+  }
+
+  // >>> متد جدیدی که باید اضافه شود:
+  Future<Map<String, int>?> getNativeData() {
+    return _platform.getNativeData();
   }
 }
